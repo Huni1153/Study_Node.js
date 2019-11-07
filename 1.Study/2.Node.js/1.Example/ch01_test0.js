ecb55d894f13da42beb4435b2208795275847d06
Huni <zxca1520@gmail.com>
2019-11-07 17:50:06 +0900

    11/7 study

---
+++ 1.Study/2.Node.js/1.Example/ch01_test0.js
@@ -0,0 +1,4 @@
+console.log('안녕하세요.');
+console.log('결과는 %d입니다.',10);
+console.log('문자열 보여주기 : %s','가나다라');
+console.log('JSON 객체 보여주기 : %j',{name:'가나다라'})
---
+++ 1.Study/2.Node.js/1.Example/ch02_test1.js
@@ -0,0 +1,29 @@
+var result = 0;
+
+console.time('duration_sum');
+for(var i = 1 ; i <= 100; i++)
+{
+    result += i;
+}
+console.timeEnd('duration_sum');
+console.log('1부터 100까지 더한 결과물 : %d',result);
+
+console.log( '현재 실행한 파일의 이름 : %s ' , __filename);
+console.log( '현재 실행한 파일의 패스 : %s ' , __dirname);
+
+var Person1 = {name:"박세훈",age:29};
+//var Person2 = {name:"박초이",age:25};
+Person1 = {name:"박초이",age:25};
+console.dir(Person1);
+//console.dir(Person2);
+
+console.log('argv 속성의 파라미터 수 : ' + process.argv.length);
+console.dir(process.argv);
+
+if(process.argv.length > 2)
+{
+    console.log('세 번째 파라미터의 값 : %s',process.argv[2]);
+}
+process.argv.forEach(function(item, index){
+    console.log(index + ' : ',item);
+});
