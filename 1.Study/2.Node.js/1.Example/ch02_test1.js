6967d87d7d8e731b3923e7353263f02753ea2885
Huni <zxca1520@gmail.com>
2019-11-11 18:13:13 +0900

    11/10 study

--- 1.Study/2.Node.js/1.Example/ch02_test1.js
+++ 1.Study/2.Node.js/1.Example/ch02_test1.js
@@ -1,4 +1,4 @@
-var result = 0;
+/*var result = 0;
 
 console.time('duration_sum');
 for(var i = 1 ; i <= 100; i++)
@@ -10,12 +10,11 @@ console.log('1부터 100까지 더한 결과물 : %d',result);
 
 console.log( '현재 실행한 파일의 이름 : %s ' , __filename);
 console.log( '현재 실행한 파일의 패스 : %s ' , __dirname);
-
-var Person1 = {name:"박세훈",age:29};
+*/
+//var Person1 = {name:"박세훈",age:29,location:"서울"};
 //var Person2 = {name:"박초이",age:25};
-Person1 = {name:"박초이",age:25};
-console.dir(Person1);
-console.dir(Person1);
+//Person1 = {name:"박초이",age:25};
+//console.dir(Person1);
 //console.dir(Person2);
 
 console.log('argv 속성의 파라미터 수 : ' + process.argv.length);
@@ -25,6 +24,6 @@ if(process.argv.length > 2)
 {
     console.log('세 번째 파라미터의 값 : %s',process.argv[2]);
 }
-process.argv.forEach(function(item, index){
+ process.argv.forEach(function(item, index){
     console.log(index + ' : ',item);
 });
---
+++ 1.Study/2.Node.js/1.Example/module_calc.js
@@ -0,0 +1,5 @@
+exports.add = function(a,b)
+{
+    console.log("여긴 module_calc.js파일");
+    return a + b;
+}
