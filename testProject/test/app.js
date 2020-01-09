fe04e5e8fea2c1984bcbd0f16eb09ece2ce2f250
Huni <zxca1520@gmail.com>
2020-01-09 21:54:48 +0900

    login view update

--- testProject/test/app.js
+++ testProject/test/app.js
@@ -11,7 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +21,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// 로그인 정보 념겨 받기
+var bodyParser=require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.post('/form_receiver',function(req,res){
+  var id=req.body.id
+  var password=req.body.password
+  res.send(id+','+password)
+  console.log(title+','+password);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- testProject/test/routes/index.js
+++ testProject/test/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Login', { title: 'Express' });
+  res.render('Login', { title: 'testProject' });
 });
 
 module.exports = router;
